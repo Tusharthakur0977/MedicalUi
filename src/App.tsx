61ad1c5fc8905999adaf04268967a4fba4004757
KartikThakur2002 <kartikthakur9735@gmail.com>
2024-04-09 20:57:45 +0530

    code refactor in app class

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import Routing from './navigation';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import SignUp from './screens/register/SignUp';
-import SignIn from './screens/login/SignIn';
-import Icon from 'react-native-vector-icons/FontAwesome'
-
-
-const Tab = createBottomTabNavigator();
 
 
 const App = () => {
   return (
     <NavigationContainer>
-     
       <Routing />
     </NavigationContainer>
   );
